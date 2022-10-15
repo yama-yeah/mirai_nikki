d3d80488209a46e2cfd0cb839c6eaea5c1c3fc2f
yama-yeah <b1021091@fun.ac.jp>
2022-10-16 03:55:05 +0900

    fix api

--- front/lib/domain/services/myApi/my_api.dart
+++ front/lib/domain/services/myApi/my_api.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
 import 'dart:convert' as convert;
 
 import 'package:http/http.dart' as http;
+import 'package:mirai_nikki/domain/model/personal_model.dart';
 import 'package:mirai_nikki/domain/model/post_model.dart';
 import 'package:mirai_nikki/domain/model/posts_model.dart';
 import 'package:mirai_nikki/domain/model/user_model.dart';
@@ -10,20 +13,21 @@ import 'package:mirai_nikki/domain/model/user_model.dart';
 import '../../util/service_util.dart';
 
 abstract class MyApiService {
-  Future<UserModel> register();
+  Future<UserModel> register(PersonalModel personalModel);
   Future<PostsModel> fetchPosts(String uid);
   Future<PostModel> post(String uid);
 }
 
 class MyApiServiceImpl implements MyApiService {
-  final URL = "https://virtserver.swaggerhub.com/cathiecode/mirai_nikki/1.0.0/";
+  final URL = "http://yogen-nikki.miselogy.miraidai.fun/";
 
   @override
-  Future<UserModel> register() async {
+  Future<UserModel> register(PersonalModel personalModel) async {
     try {
       var raw = await http.post(
         Uri.parse('${URL}user'),
         headers: {'accept': 'application/json'},
+        body: jsonEncode(personalModel.toJson()),
       );
       final json = json2map(raw);
 
@@ -39,13 +43,14 @@ class MyApiServiceImpl implements MyApiService {
   Future<PostsModel> fetchPosts(String uid) async {
     try {
       var raw = await http.get(
-        Uri.parse('${URL}posts/by-uid/${uid}'),
+        Uri.parse('${URL}posts/by-uid/$uid'),
         headers: {'accept': 'application/json'},
       );
+      //Logger().wtf(raw.body);
       final json = json2map(raw);
       return PostsModel.fromJson(json);
     } catch (e) {
-      Logger().e("Api:$e");
+      Logger().e(e);
       //throw Exception(e);
       return const PostsModel();
     }
