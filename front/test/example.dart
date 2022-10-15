ab3f0a779d01ed867b79d6fbaa9c8ccf2863a3b5
yama-yeah <b1021091@fun.ac.jp>
2022-10-15 14:33:24 +0900

    add domain & example

---
+++ front/test/example.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:logger/logger.dart';
+
+void main() {
+  test('tasks dao test', () async {
+    Logger().d('hello world');
+  });
+}
