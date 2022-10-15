62db5397f769443cea9d8c5e0c0d5b6bb32bc3b2
yama-yeah <b1021091@fun.ac.jp>
2022-10-16 03:53:35 +0900

    fix initialize

--- front/lib/initialize.dart
+++ front/lib/initialize.dart
@@ -1,6 +1,8 @@
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mirai_nikki/domain/services/myApi/my_api.dart';
+import 'package:mirai_nikki/domain/services/secure/secure.dart';
 import 'package:mirai_nikki/domain/state/user_state.dart';
 
 import 'domain/model/user_model.dart';
@@ -8,14 +10,16 @@ import 'domain/model/user_model.dart';
 class InitializeStateNotifier extends StateNotifier<bool> {
   final StateController<UserModel> userStateController;
   final MyApiService api;
-  InitializeStateNotifier(this.userStateController, this.api) : super(false);
+  final SecureStorage secure;
+  InitializeStateNotifier(this.userStateController, this.api, this.secure)
+      : super(false);
   void initialize() async {
-    final uid = await const FlutterSecureStorage().read(key: 'uid');
-    if (uid == null) {
-      final model = await api.register();
-      userStateController.state = model;
+    final user = await secure.loadUser();
+    if (user == const UserModel()) {
+      //final model = await api.register();
+      //userStateController.state = model;
     } else {
-      userStateController.state = UserModel(uid: uid);
+      userStateController.state = user;
     }
     state = true;
   }
@@ -25,6 +29,8 @@ final initializeStateNotifierProvider =
     StateNotifierProvider<InitializeStateNotifier, bool>(
   (ref) {
     final userStateController = ref.read(userStateProvider.notifier);
-    return InitializeStateNotifier(userStateController, MyApiServiceImpl());
+    final api = ref.watch(myApiProvider);
+    final secure = ref.watch(secureStorageProvider);
+    return InitializeStateNotifier(userStateController, api, secure);
   },
 );
